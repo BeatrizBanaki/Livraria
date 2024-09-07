661068c8e8fc1ec7066b6291069e44ed7033c05a
Beatriz Banaki <112290703+BeatrizBanaki@users.noreply.github.com>
2024-09-06 22:51:28 -0300

    cria rota valor

--- routes/editoras.js
+++ routes/editoras.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { criarEditora, atualizarEditora, excluirEditora, getEditoras, getEditora } = require('../controllers/editoraController');
+const { criarEditora, atualizarEditora, excluirEditora, getEditoras, getEditora, valorLivros } = require('../controllers/editoraController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
+
 
 router.get('/editoras/:id', authMiddleware, getEditora)
 router.get('/editoras', authMiddleware, getEditoras)
 router.post('/editoras', authMiddleware, criarEditora);
 router.delete('/editoras/:id', authMiddleware, excluirEditora);
 router.put('/editoras/:id', authMiddleware, atualizarEditora);
+router.get('/editoras/valor-livros/:id', authMiddleware, valorLivros);
 
 module.exports = router;
